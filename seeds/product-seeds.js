8a5076ccfa7ef385e2dea858c49d8ce8845b9713
quicksilver524 <quicksilver524@gmail.com>
2022-07-15 15:19:57 -0400

    updated expanded seeds

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -2,32 +2,32 @@ const { Product } = require("../models");
 
 const productData = [
   {
-    product_name: "Plain T-Shirt",
-    price: 14.99,
+    product_name: "T-Shirts",
+    price: 12.99,
     stock: 14,
     category_id: 1,
   },
   {
-    product_name: "Running Sneakers",
-    price: 90.0,
+    product_name: "Sneakers",
+    price: 120.0,
     stock: 25,
     category_id: 5,
   },
   {
-    product_name: "Branded Baseball Hat",
-    price: 22.99,
+    product_name: "Fitted Hats",
+    price: 24.99,
     stock: 12,
     category_id: 4,
   },
   {
-    product_name: "Top 40 Music Compilation Vinyl Record",
-    price: 12.99,
+    product_name: "Music Compilation Discs",
+    price: 15.99,
     stock: 50,
     category_id: 3,
   },
   {
-    product_name: "Cargo Shorts",
-    price: 29.99,
+    product_name: "Board Shorts",
+    price: 24.99,
     stock: 22,
     category_id: 2,
   },
